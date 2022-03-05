061097dd11f949864974d636b31942bcc2f5cb3c
Gibdos <simon.yousefi@protonmail.com>
2022-03-05 08:09:32 +0100

    Basic game structure and logic done

---
+++ script.js
@@ -0,0 +1,74 @@
+const game = {
+	gameBoard: ["", "", "", "", "", "", "", "", ""],
+	gameStart() {
+		playerOne.isRound = true;
+	},
+	switchPlayer() {
+		playerOne.isRound = !playerOne.isRound;
+		playerTwo.isRound = !playerTwo.isRound;
+	},
+	checkRound() {
+		let hState1 = this.gameBoard[0] + game.gameBoard[1] + game.gameBoard[2];
+		let hState2 = this.gameBoard[3] + game.gameBoard[4] + game.gameBoard[5];
+		let hState3 = this.gameBoard[6] + game.gameBoard[7] + game.gameBoard[8];
+		let vState1 = this.gameBoard[0] + game.gameBoard[3] + game.gameBoard[6];
+		let vState2 = this.gameBoard[1] + game.gameBoard[4] + game.gameBoard[7];
+		let vState3 = this.gameBoard[2] + game.gameBoard[5] + game.gameBoard[8];
+		let dState1 = this.gameBoard[0] + game.gameBoard[4] + game.gameBoard[8];
+		let dState2 = this.gameBoard[2] + game.gameBoard[4] + game.gameBoard[6];
+		if (hState1 === "XXX" || hState2 === "XXX" || hState3 === "XXX" || vState1 === "XXX" || vState2 === "XXX" || vState3 === "XXX" || dState1 === "XXX" || dState2 === "XXX") {
+			alert("PLAYER ONE WINS");
+		} else if (hState1 === "OOO" || hState2 === "OOO" || hState3 === "OOO" || vState1 === "OOO" || vState2 === "OOO" || vState3 === "OOO" || dState1 === "OOO" || dState2 === "OOO") {
+			alert("PLAYER TWO WINS");
+		} else if (this.gameBoard.includes("")) {
+		} else {
+			alert("THAT IS A TIE!");
+		}
+	},
+};
+
+function playerCreator(name, marker) {
+	(this.name = name), (this.marker = marker), (this.score = 0), (this.isRound = false);
+}
+
+const displayController = {
+	reset: function resetField() {
+		const gameFieldBlocks = document.querySelectorAll(".field-block");
+		gameFieldBlocks.forEach((element) => {
+			element.innerHTML = "";
+		});
+	},
+
+	createHTML(clickedField) {
+		if (playerOne.isRound) {
+			const Block = document.createElement("div");
+			Block.classList.add("X");
+			this.addHTML(clickedField, Block);
+		} else {
+			const Block = document.createElement("div");
+			Block.classList.add("O");
+			this.addHTML(clickedField, Block);
+		}
+	},
+	addHTML(clickedField, block) {
+		const setPiece = clickedField.target.classList[0] - 1;
+		const marker = block.classList[0];
+		console.log(clickedField.target, block);
+		clickedField.target.appendChild(block);
+		game.gameBoard[setPiece] = marker;
+		game.switchPlayer();
+		game.checkRound();
+	},
+};
+
+const eventHandler = document.querySelectorAll(".field-block");
+eventHandler.forEach((element) => {
+	element.addEventListener("click", (e) => {
+		displayController.createHTML(e);
+	});
+});
+
+// Initialize Game
+const playerOne = new playerCreator("PlayerOne", "X");
+const playerTwo = new playerCreator("PlayerTwo", "O");
+game.gameStart();
